9cf24a5d10306bcf369fb22fed6a55b7d9bac68b
Nick Wang <ylw238@nyu.edu>
2020-07-07 17:28:46 -0400

    modified md files for lint

--- cmd/cost/cost.go
+++ cmd/cost/cost.go
@@ -100,17 +100,8 @@ func (opsCost *costOptions) initAWSClients() (awsprovider.OrganizationsClient, a
 var CostCmd = &cobra.Command{
 	Use:   "cost",
 	Short: "Cost Management related utilities",
-	Long: `The cost command allows for cost management on the AWS platform (other
-platforms may be added in the future. Its functions include:
-
-- Managing the AWS Cost Explorer with $ osdctl cost aws. This leaves the possibility of adding cost 
-management support for other platforms e.g. $ osdctl cost gcp
-
-- Get cost of OUs with $ osdctl cost aws get
-
-- Create cost category with $ osdctl cost aws create
-
-- Reconcile cost categories with $ osdctl cost aws reconcile`,
+	Long: `The cost command allows for cost management on the AWS platform (other 
+platforms may be added in the future)`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("cost called")
 	},
