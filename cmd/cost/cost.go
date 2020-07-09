3f1adf8f9eafd30ffe4baacd4460014126efded8
Nick Wang <ylw238@nyu.edu>
2020-07-09 11:34:04 -0400

    Made function NewCmdCost and used it instead of variable CostCmd to add command in root.go

--- cmd/cost/cost.go
+++ cmd/cost/cost.go
@@ -3,19 +3,24 @@ package cost
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 // costCmd represents the cost command
-var CostCmd = &cobra.Command{
-	Use:   "cost",
-	Short: "Cost Management related utilities",
-	Long: `The cost command allows for cost management on the AWS platform (other 
+func NewCmdCost(streams genericclioptions.IOStreams) *cobra.Command {
+	costCmd := &cobra.Command{
+		Use:   "cost",
+		Short: "Cost Management related utilities",
+		Long: `The cost command allows for cost management on the AWS platform (other 
 platforms may be added in the future)`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cost called")
-	},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("cost called")
+		},
+	}
+
+	costCmd.AddCommand(awsCmd)
+
+	return costCmd
 }
 
-func init() {
-	CostCmd.AddCommand(awsCmd)
-}
+
