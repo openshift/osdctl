0f99ece32299bbaaab8977678b6fd11183e33c1a
Nick Wang <ylw238@nyu.edu>
2020-07-15 14:19:34 -0400

    Created interfaces OrganizationsClients and CostExplorerClient and changed necessary parameters of all files to reflect this

--- cmd/cost/cost.go
+++ cmd/cost/cost.go
@@ -2,8 +2,6 @@ package cost
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/service/costexplorer"
-	"github.com/aws/aws-sdk-go/service/organizations"
 	"github.com/openshift/osd-utils-cli/cmd/common"
 	awsprovider "github.com/openshift/osd-utils-cli/pkg/provider/aws"
 	"github.com/spf13/cobra"
@@ -14,24 +12,22 @@ import (
 
 // costCmd represents the cost command
 func NewCmdCost(streams genericclioptions.IOStreams) *cobra.Command {
-	ops := newCostOptions(streams)
+	opsCost = newCostOptions(streams)
 	costCmd := &cobra.Command{
 		Use:   "cost",
 		Short: "Cost Management related utilities",
 		Long: `The cost command allows for cost management on the AWS platform (other 
 platforms may be added in the future)`,
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			cmdutil.CheckErr(ops.complete(cmd, args))
-			cmdutil.CheckErr(ops.initAWSClients())
 		},
 	}
 
 	//Set flags
-	costCmd.PersistentFlags().StringVarP(&ops.accessKeyID, "aws-access-key-id", "a", "", "AWS Access Key ID")
-	costCmd.PersistentFlags().StringVarP(&ops.secretAccessKey, "aws-secret-access-key", "x", "", "AWS Secret Access Key")
-	costCmd.PersistentFlags().StringVarP(&ops.profile, "aws-profile", "p", "", "specify AWS profile")
-	costCmd.PersistentFlags().StringVarP(&ops.configFile, "aws-config", "c", "", "specify AWS config file path")
-	costCmd.PersistentFlags().StringVarP(&ops.region, "aws-region", "g", common.DefaultRegion, "specify AWS region")
+	costCmd.PersistentFlags().StringVarP(&opsCost.accessKeyID, "aws-access-key-id", "a", "", "AWS Access Key ID")
+	costCmd.PersistentFlags().StringVarP(&opsCost.secretAccessKey, "aws-secret-access-key", "x", "", "AWS Secret Access Key")
+	costCmd.PersistentFlags().StringVarP(&opsCost.profile, "aws-profile", "p", "", "specify AWS profile")
+	costCmd.PersistentFlags().StringVarP(&opsCost.configFile, "aws-config", "c", "", "specify AWS config file path")
+	costCmd.PersistentFlags().StringVarP(&opsCost.region, "aws-region", "g", common.DefaultRegion, "specify AWS region")
 
 	//Add commands
 	costCmd.AddCommand(newCmdGet(streams))
@@ -41,8 +37,22 @@ platforms may be added in the future)`,
 	return costCmd
 }
 
-var org *organizations.Organizations
-var ce *costexplorer.CostExplorer
+var opsCost *costOptions
+
+//var org *organizations.Organizations
+//var ce *costexplorer.CostExplorer
+
+
+//For interfaces-------------------------------------------------
+//var orgClient awsprovider.OrganizationsClient
+//var ceClient awsprovider.CostExplorerI
+
+//func init() {
+//	orgClient = &organizations.Organizations{}
+//	ceClient = &costexplorer.CostExplorer{}
+//}
+//----------------------------------------------------------------
+
 
 // costOptions defines the struct for running Cost command
 type costOptions struct {
@@ -62,20 +72,20 @@ func newCostOptions(streams genericclioptions.IOStreams) *costOptions {
 	}
 }
 
-func (ops *costOptions) initAWSClients() error {
+func (opsCost *costOptions) initAWSClients() (awsprovider.OrganizationsClient, awsprovider.CostExplorerClient, error) {
 	//Initialize AWS clients
 	var (
 		awsClient awsprovider.Client
 		err       error
 	)
 
-	if ops.accessKeyID == "" && ops.secretAccessKey == "" {
-		awsClient, err = awsprovider.NewAwsClient(ops.profile, ops.region, ops.configFile)
+	if opsCost.accessKeyID == "" && opsCost.secretAccessKey == "" {
+		awsClient, err = awsprovider.NewAwsClient(opsCost.profile, opsCost.region, opsCost.configFile)
 	} else {
 		awsClient, err = awsprovider.NewAwsClientWithInput(&awsprovider.AwsClientInput{
-			AwsIDKey:     ops.accessKeyID,
-			AwsAccessKey: ops.secretAccessKey,
-			AwsRegion:    ops.region,
+			AwsIDKey:     opsCost.accessKeyID,
+			AwsAccessKey: opsCost.secretAccessKey,
+			AwsRegion:    opsCost.region,
 		})
 	}
 
@@ -83,16 +93,13 @@ func (ops *costOptions) initAWSClients() error {
 		log.Fatalln("Error getting AWS clients:", err)
 	}
 
-	org = awsClient.GetOrg()
-	ce = awsClient.GetCE()
-
-	return err
+	return awsClient.GetOrg(), awsClient.GetCE(), err
 }
 
-func (ops *costOptions) complete(cmd *cobra.Command, _ []string) error {
-	if ops.accessKeyID == "" && ops.secretAccessKey == "" {
-		fmt.Fprintln(ops.Out, "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are not provided, reading credentials from config file or env vars.")
-	} else if ops.accessKeyID == "" || ops.secretAccessKey == "" {
+func (opsCost *costOptions) complete(cmd *cobra.Command, _ []string) error {
+	if opsCost.accessKeyID == "" && opsCost.secretAccessKey == "" {
+		fmt.Fprintln(opsCost.Out, "AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are not provided, reading credentials from config file or env vars.")
+	} else if opsCost.accessKeyID == "" || opsCost.secretAccessKey == "" {
 		return cmdutil.UsageErrorf(cmd, "The flag aws-access-key-id and aws-secret-access-key should be set or not set at the same time")
 	}
 
