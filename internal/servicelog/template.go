c8ceb574cf1594fb303cb18cf34de43607095542
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-06-10 17:03:07 +0300

    feat(servicelog): add eventstreamid
    this will post the eventstreamid to the servicelog api

--- internal/servicelog/template.go
+++ internal/servicelog/template.go
@@ -7,12 +7,13 @@ import (
 
 // Message is the base template structure
 type Message struct {
-	Severity     string `json:"severity"`
-	ServiceName  string `json:"service_name"`
-	ClusterUUID  string `json:"cluster_uuid"`
-	Summary      string `json:"summary"`
-	Description  string `json:"description"`
-	InternalOnly bool   `json:"internal_only"`
+	Severity      string `json:"severity"`
+	ServiceName   string `json:"service_name"`
+	ClusterUUID   string `json:"cluster_uuid"`
+	Summary       string `json:"summary"`
+	Description   string `json:"description"`
+	InternalOnly  bool   `json:"internal_only"`
+	EventStreamID string `json:"event_stream_id"`
 }
 
 func (m *Message) GetSeverity() string {
@@ -39,12 +40,17 @@ func (m *Message) GetInternalOnly() bool {
 	return m.InternalOnly
 }
 
+func (m *Message) GetEventStreamID() string {
+	return m.EventStreamID
+}
+
 func (m *Message) ReplaceWithFlag(variable, value string) {
 	m.Severity = strings.ReplaceAll(m.Severity, variable, value)
 	m.ServiceName = strings.ReplaceAll(m.ServiceName, variable, value)
 	m.ClusterUUID = strings.ReplaceAll(m.ClusterUUID, variable, value)
 	m.Summary = strings.ReplaceAll(m.Summary, variable, value)
 	m.Description = strings.ReplaceAll(m.Description, variable, value)
+	m.EventStreamID = strings.ReplaceAll(m.EventStreamID, variable, value)
 }
 
 func (m *Message) SearchFlag(placeholder string) (found bool) {
@@ -63,12 +69,15 @@ func (m *Message) SearchFlag(placeholder string) (found bool) {
 	if found = strings.Contains(m.Description, placeholder); found == true {
 		return found
 	}
+	if found = strings.Contains(m.EventStreamID, placeholder); found == true {
+		return found
+	}
 	return false
 }
 
 func (m *Message) FindLeftovers() (matches []string, found bool) {
 	r := regexp.MustCompile(`\${[^{}]*}`)
-	str := m.Severity + m.ServiceName + m.ClusterUUID + m.Summary + m.Description
+	str := m.Severity + m.ServiceName + m.ClusterUUID + m.Summary + m.Description + m.EventStreamID
 	matches = r.FindAllString(str, -1)
 	if len(matches) > 0 {
 		found = true
