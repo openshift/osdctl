079db2dd8d1062ecc4b406bf38c57f5cec4f257f
Katherine Lin <klin@redhat.com>
2021-07-13 16:34:22 -0400

    Refactor into functions

--- cmd/account/mgmt/account-unassign.go
+++ cmd/account/mgmt/account-unassign.go
@@ -38,6 +38,7 @@ func newCmdAccountUnassign(streams genericclioptions.IOStreams, flags *genericcl
 }
 
 type accountUnassignOptions struct {
+	awsClient    awsprovider.Client
 	username     string
 	payerAccount string
 	accountID    string
@@ -65,6 +66,103 @@ func (o *accountUnassignOptions) complete(cmd *cobra.Command, _ []string) error 
 	return nil
 }
 
+func (o *accountUnassignOptions) run() error {
+
+	var (
+		accountUsername string
+		accountIdList   []string
+		destinationOU   string
+		rootID          string
+	)
+
+	// Instantiate Aws client
+	awsClient, err := awsprovider.NewAwsClient(o.payerAccount, "us-east-1", "")
+	if err != nil {
+		return err
+	}
+
+	o.awsClient = awsClient
+
+	if o.accountID != "" {
+
+		accountIdList = append(accountIdList, o.accountID)
+
+	}
+
+	if o.username != "" {
+		// Check that username is not a hive
+		if strings.HasPrefix(o.username, "hive") {
+			return fmt.Errorf("non-ccs account provided, only developers account accepted")
+		}
+
+		accountUsername = o.username
+		accountIdList, err = o.listAccountsFromUser(accountUsername)
+		if err != nil {
+			return err
+		}
+	}
+
+	c := askForConfirmation("Are you sure you want to unassign the accounts? y/n")
+	if c {
+		// Delete login profile
+		err := o.deleteLoginProfile(accountUsername)
+		if err != nil {
+			return err
+		}
+		// Delete access keys
+		err = o.deleteAccessKeys(accountUsername)
+		if err != nil {
+			return err
+		}
+		// Delete signing certificates
+		err = o.deleteSigningCert(accountUsername)
+		if err != nil {
+			return err
+		}
+		// Delete policies
+		err = o.deletePolicies(accountUsername)
+		if err != nil {
+			return err
+		}
+		// Delete attached policies
+		err = o.deleteAttachedPolicies(accountUsername)
+		if err != nil {
+			return err
+		}
+		// Delete groups
+		err = o.deleteLoginProfile(accountUsername)
+		if err != nil {
+			return err
+		}
+		// Delete user
+		err = o.deleteUser(accountUsername)
+		if err != nil {
+			return err
+		}
+		// loop through accounts list and untag and move them back into root OU
+		for _, id := range accountIdList {
+
+			err = o.untagAccount(id)
+			if err != nil {
+				return err
+			}
+
+			destinationOU = "r-rs3h-ry0hn2l9"
+			rootID = "r-rs3h"
+			if o.payerAccount == "osd-staging-1" {
+				destinationOU = "ou-0wd6-z6tzkjek"
+				rootID = "ou-0wd6"
+			}
+
+			err = o.moveAccount(id, rootID, destinationOU)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 func askForConfirmation(s string) bool {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -86,205 +184,197 @@ func askForConfirmation(s string) bool {
 	}
 }
 
-func (o *accountUnassignOptions) run() error {
+var ErrNoAccountsForUser error = fmt.Errorf("user has no aws accounts")
 
-	var (
-		accountUsername string
-		accountIdList   []string
-		destinationOU   string
-		rootID          string
-	)
+func (o *accountUnassignOptions) listAccountsFromUser(user string) ([]string, error) {
 
-	// Instantiate Aws client
-	awsClient, err := awsprovider.NewAwsClient(o.payerAccount, "us-east-1", "")
+	inputFilterTag := &resourcegroupstaggingapi.GetResourcesInput{
+		TagFilters: []*resourcegroupstaggingapi.TagFilter{
+			{
+				Key: aws.String("owner"),
+				Values: []*string{
+					aws.String(user),
+				},
+			},
+		},
+	}
+	accounts, err := o.awsClient.GetResources(inputFilterTag)
+	if err != nil {
+		return []string{}, err
+	}
+	var accountIdList []string
+	// Get last 12 digits of ResourceARN and append it to account list
+	for _, a := range accounts.ResourceTagMappingList {
+		accountIdList = append(accountIdList, (*a.ResourceARN)[len(*a.ResourceARN)-12:])
+	}
+	if err != nil {
+		return []string{}, err
+	}
+	return accountIdList, ErrNoAccountsForUser
+}
+
+func (o *accountUnassignOptions) deleteLoginProfile(user string) error {
+
+	inputDeleteLogin := &iam.DeleteLoginProfileInput{
+		UserName: &user,
+	}
+	_, err := o.awsClient.DeleteLoginProfile(inputDeleteLogin)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *accountUnassignOptions) deleteAccessKeys(user string) error {
+
+	inputListAccessKeys := &iam.ListAccessKeysInput{
+		UserName: &user,
+	}
+	accessKeys, err := o.awsClient.ListAccessKeys(inputListAccessKeys)
 	if err != nil {
 		return err
 	}
 
-	if o.accountID != "" {
+	for _, m := range accessKeys.AccessKeyMetadata {
 
-		accountIdList = append(accountIdList, o.accountID)
-
-	}
-
-	if o.username != "" {
-		// Check that username is not a hive
-		if strings.HasPrefix(o.username, "hive") {
-			return fmt.Errorf("non-ccs account provided, only developers account accepted")
+		inputDelKey := &iam.DeleteAccessKeyInput{
+			AccessKeyId: m.AccessKeyId,
+			UserName:    &user,
 		}
-
-		accountUsername = o.username
-
-		// Get all accounts from user
-		inputFilterTag := &resourcegroupstaggingapi.GetResourcesInput{
-			TagFilters: []*resourcegroupstaggingapi.TagFilter{
-				{
-					Key: aws.String("owner"),
-					Values: []*string{
-						aws.String(accountUsername),
-					},
-				},
-			},
-		}
-		accounts, err := awsClient.GetResources(inputFilterTag)
-		if err != nil {
-			return err
-		}
-		// Get last 12 digits of ResourceARN and append it to account list
-		for _, a := range accounts.ResourceTagMappingList {
-			accountIdList = append(accountIdList, (*a.ResourceARN)[len(*a.ResourceARN)-12:])
-		}
+		_, err = o.awsClient.DeleteAccessKey(inputDelKey)
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
 
-	c := askForConfirmation("Are you sure you want to unassign the accounts? y/n")
-	if c {
-		// Delete login profile
-		inputDeleteLogin := &iam.DeleteLoginProfileInput{
-			UserName: &accountUsername,
+func (o *accountUnassignOptions) deleteSigningCert(user string) error {
+
+	inputListCert := &iam.ListSigningCertificatesInput{
+		UserName: &user,
+	}
+	cert, err := o.awsClient.ListSigningCertificates(inputListCert)
+	if err != nil {
+		return err
+	}
+
+	for _, c := range cert.Certificates {
+		inputDelCert := &iam.DeleteSigningCertificateInput{
+			CertificateId: c.CertificateId,
+			UserName:      &user,
 		}
-		_, err = awsClient.DeleteLoginProfile(inputDeleteLogin)
+		_, err = o.awsClient.DeleteSigningCertificate(inputDelCert)
 		if err != nil {
 			return err
 		}
-		// Delete access keys
-		inputListAccessKeys := &iam.ListAccessKeysInput{
-			UserName: &accountUsername,
+	}
+	return nil
+}
+
+func (o *accountUnassignOptions) deletePolicies(user string) error {
+	inputListPolicies := &iam.ListUserPoliciesInput{
+		UserName: &user,
+	}
+	policies, err := o.awsClient.ListUserPolicies(inputListPolicies)
+	if err != nil {
+		return err
+	}
+
+	for _, p := range policies.PolicyNames {
+		inputDelPolicies := &iam.DeleteUserPolicyInput{
+			PolicyName: p,
+			UserName:   &user,
 		}
-		accessKeys, err := awsClient.ListAccessKeys(inputListAccessKeys)
+		_, err = o.awsClient.DeleteUserPolicy(inputDelPolicies)
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
 
-		for _, m := range accessKeys.AccessKeyMetadata {
-			inputDelKey := &iam.DeleteAccessKeyInput{
-				AccessKeyId: m.AccessKeyId,
-				UserName:    &accountUsername,
-			}
-			_, err = awsClient.DeleteAccessKey(inputDelKey)
-			if err != nil {
-				return err
-			}
+func (o *accountUnassignOptions) deleteAttachedPolicies(user string) error {
+	inputListAttachedPol := &iam.ListAttachedUserPoliciesInput{
+		UserName: &user,
+	}
+	attachedPol, err := o.awsClient.ListAttachedUserPolicies(inputListAttachedPol)
+	if err != nil {
+		return err
+	}
+
+	for _, ap := range attachedPol.AttachedPolicies {
+		inputDetachPol := &iam.DetachUserPolicyInput{
+			PolicyArn: ap.PolicyArn,
+			UserName:  &user,
 		}
-		// Delete signing certificates
-		inputListCert := &iam.ListSigningCertificatesInput{
-			UserName: &accountUsername,
-		}
-		cert, err := awsClient.ListSigningCertificates(inputListCert)
+		_, err = o.awsClient.DetachUserPolicy(inputDetachPol)
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
 
-		for _, c := range cert.Certificates {
-			inputDelCert := &iam.DeleteSigningCertificateInput{
-				CertificateId: c.CertificateId,
-				UserName:      &accountUsername,
-			}
-			_, err = awsClient.DeleteSigningCertificate(inputDelCert)
-			if err != nil {
-				return err
-			}
+func (o *accountUnassignOptions) deleteGroups(user string) error {
+
+	inputListGroups := &iam.ListGroupsForUserInput{
+		UserName: &user,
+	}
+	groups, err := o.awsClient.ListGroupsForUser(inputListGroups)
+	if err != nil {
+		return err
+	}
+
+	for _, g := range groups.Groups {
+		inputRemoveFromGroup := &iam.RemoveUserFromGroupInput{
+			GroupName: g.GroupName,
+			UserName:  &user,
 		}
-		// Delete policies
-		inputListPolicies := &iam.ListUserPoliciesInput{
-			UserName: &accountUsername,
-		}
-		policies, err := awsClient.ListUserPolicies(inputListPolicies)
+		_, err = o.awsClient.RemoveUserFromGroup(inputRemoveFromGroup)
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
 
-		for _, p := range policies.PolicyNames {
-			inputDelPolicies := &iam.DeleteUserPolicyInput{
-				PolicyName: p,
-				UserName:   &accountUsername,
-			}
-			_, err = awsClient.DeleteUserPolicy(inputDelPolicies)
-			if err != nil {
-				return err
-			}
-		}
-		// Delete attached policies
-		inputListAttachedPol := &iam.ListAttachedUserPoliciesInput{
-			UserName: &accountUsername,
-		}
-		attachedPol, err := awsClient.ListAttachedUserPolicies(inputListAttachedPol)
-		if err != nil {
-			return err
-		}
+func (o *accountUnassignOptions) deleteUser(user string) error {
+	inputDelUser := &iam.DeleteUserInput{
+		UserName: &user,
+	}
+	_, err := o.awsClient.DeleteUser(inputDelUser)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-		for _, ap := range attachedPol.AttachedPolicies {
-			inputDetachPol := &iam.DetachUserPolicyInput{
-				PolicyArn: ap.PolicyArn,
-				UserName:  &accountUsername,
-			}
-			_, err = awsClient.DetachUserPolicy(inputDetachPol)
-			if err != nil {
-				return err
-			}
-		}
-		// Delete groups
-		inputListGroups := &iam.ListGroupsForUserInput{
-			UserName: &accountUsername,
-		}
-		groups, err := awsClient.ListGroupsForUser(inputListGroups)
-		if err != nil {
-			return err
-		}
+func (o *accountUnassignOptions) untagAccount(id string) error {
+	inputUntag := &organizations.UntagResourceInput{
+		ResourceId: &id,
+		TagKeys: []*string{
+			aws.String("owner"),
+			aws.String("claimed"),
+		},
+	}
+	_, err := o.awsClient.UntagResource(inputUntag)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
-		for _, g := range groups.Groups {
-			inputRemoveFromGroup := &iam.RemoveUserFromGroupInput{
-				GroupName: g.GroupName,
-				UserName:  &accountUsername,
-			}
-			_, err = awsClient.RemoveUserFromGroup(inputRemoveFromGroup)
-			if err != nil {
-				return err
-			}
-		}
-		// Delete user
-		inputDelUser := &iam.DeleteUserInput{
-			UserName: &accountUsername,
-		}
-		_, err = awsClient.DeleteUser(inputDelUser)
-		if err != nil {
-			return err
-		}
-		// loop through accounts list and untag and move them back into root OU
-		for _, id := range accountIdList {
+func (o *accountUnassignOptions) moveAccount(id string, rootID string, destinationOU string) error {
+	inputMove := &organizations.MoveAccountInput{
+		AccountId:           aws.String(id),
+		DestinationParentId: aws.String(rootID),
+		SourceParentId:      aws.String(destinationOU),
+	}
 
-			inputUntag := &organizations.UntagResourceInput{
-				ResourceId: &id,
-				TagKeys: []*string{
-					aws.String("owner"),
-					aws.String("claimed"),
-				},
-			}
-			_, err = awsClient.UntagResource(inputUntag)
-			if err != nil {
-				return err
-			}
-
-			destinationOU = "r-rs3h-ry0hn2l9"
-			rootID = "r-rs3h"
-			if o.payerAccount == "osd-staging-1" {
-				destinationOU = "ou-0wd6-z6tzkjek"
-				rootID = "ou-0wd6"
-			}
-
-			inputMove := &organizations.MoveAccountInput{
-				AccountId:           aws.String(id),
-				DestinationParentId: aws.String(rootID),
-				SourceParentId:      aws.String(destinationOU),
-			}
-
-			_, err = awsClient.MoveAccount(inputMove)
-			if err != nil {
-				return err
-			}
-		}
+	_, err := o.awsClient.MoveAccount(inputMove)
+	if err != nil {
+		return err
 	}
 	return nil
 }
