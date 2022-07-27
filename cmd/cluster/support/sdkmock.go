357dff85e273872a110ed777912693c4a432f987
Luis Falcon <lfalconm@redhat.com>
2022-07-27 11:08:23 -0400

    mock sdk connection basic requests, refactor code to accomodate api mocking for unit tests, added validation to clusterID format before sending requests to API

---
+++ cmd/cluster/support/sdkmock.go
@@ -0,0 +1,27 @@
+package support
+
+import sdk "github.com/openshift-online/ocm-sdk-go"
+
+type SDKConnection interface {
+	Post() *sdk.Request
+	Delete() *sdk.Request
+}
+
+var (
+	Client SDKConnection
+)
+
+//sdk client structure
+type MockClient struct {
+	//empty structure to satisfy interface
+}
+
+//Mock POST request to the API for unit tests
+func (m *MockClient) Post() *sdk.Request {
+	return &sdk.Request{}
+}
+
+//Mock Delete request to the API for unit tests
+func (m *MockClient) Delete() *sdk.Request {
+	return &sdk.Request{}
+}
