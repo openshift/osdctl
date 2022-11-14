6abead698895d2796d5e90277ed7d28018013120
Taylor Fahlman <tfahlman@redhat.com>
2022-11-14 14:40:19 -0500

    Add protection when running any command to ensure that user intends to run a version of osdctl that does not match the latest tagged release

--- cmd/version.go
+++ cmd/version.go
@@ -3,32 +3,12 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"runtime/debug"
 	"strings"
-	"time"
 
+	"github.com/openshift/osdctl/pkg/utils"
 	"github.com/spf13/cobra"
 )
-
-const (
-	versionAPIEndpoint     = "https://api.github.com/repos/openshift/osdctl/releases/latest"
-	versionAddressTemplate = "https://github.com/openshift/osdctl/releases/download/v%s/osdctl_%s_%s_%s.tar.gz" // version, version, GOOS, GOARCH
-)
-
-var (
-	// Version is the tag version from the environment
-	// Will be set during build process via GoReleaser
-	// See also: https://pkg.go.dev/cmd/link
-	Version string
-)
-
-// githubResponse is a necessary struct for the JSON unmarshalling that is happening
-// in the getLatestVersion().
-type gitHubResponse struct {
-	TagName string `json:"tag_name"`
-}
 
 // versionResponse is necessary for the JSON version response. It uses the three
 // variables that get set during the build.
@@ -59,10 +39,10 @@ func version(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	latest, _ := getLatestVersion() // let's ignore this error, just in case we have no internet access
+	latest, _ := utils.GetLatestVersion() // let's ignore this error, just in case we have no internet access
 	ver, err := json.MarshalIndent(&versionResponse{
 		Commit:  gitCommit,
-		Version: Version,
+		Version: utils.Version,
 		Latest:  strings.TrimPrefix(latest, "v"),
 	}, "", "  ")
 	if err != nil {
@@ -70,36 +50,4 @@ func version(cmd *cobra.Command, args []string) error {
 	}
 	fmt.Println(string(ver))
 	return nil
-}
-
-// getLatestVersion connects to the GitHub API and returns the latest osdctl tag name
-// Interesting Note: GitHub only shows the latest "stable" tag. This means, that
-// tags with a suffix like *-rc.1 are not returned. We will always show the latest stable on master branch.
-func getLatestVersion() (latest string, err error) {
-	client := http.Client{
-		Timeout: time.Second * 2,
-	}
-
-	req, err := http.NewRequest(http.MethodGet, versionAPIEndpoint, nil)
-	if err != nil {
-		return latest, err
-	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		return latest, err
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return latest, err
-	}
-
-	githubResp := gitHubResponse{}
-	err = json.Unmarshal(body, &githubResp)
-	if err != nil {
-		return latest, err
-	}
-
-	return githubResp.TagName, nil
 }
---
+++ pkg/utils/version.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
+
+const (
+	VersionAPIEndpoint     = "https://api.github.com/repos/openshift/osdctl/releases/latest"
+	VersionAddressTemplate = "https://github.com/openshift/osdctl/releases/download/v%s/osdctl_%s_%s_%s.tar.gz" // version, version, GOOS, GOARCH
+)
+
+var (
+	// GitCommit is the short git commit hash from the environment
+	// Will be set during build process via GoReleaser
+	// See also: https://pkg.go.dev/cmd/link
+	GitCommit string
+
+	// Version is the tag version from the environment
+	// Will be set during build process via GoReleaser
+	// See also: https://pkg.go.dev/cmd/link
+	Version string
+)
+
+// githubResponse is a necessary struct for the JSON unmarshalling that is happening
+// in the getLatestVersion().
+type gitHubResponse struct {
+	TagName string `json:"tag_name"`
+}
+
+// getLatestVersion connects to the GitHub API and returns the latest osdctl tag name
+// Interesting Note: GitHub only shows the latest "stable" tag. This means, that
+// tags with a suffix like *-rc.1 are not returned. We will always show the latest stable on master branch.
+func GetLatestVersion() (latest string, err error) {
+	client := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, VersionAPIEndpoint, nil)
+	if err != nil {
+		return latest, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return latest, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return latest, err
+	}
+
+	githubResp := gitHubResponse{}
+	err = json.Unmarshal(body, &githubResp)
+	if err != nil {
+		return latest, err
+	}
+
+	return githubResp.TagName, nil
+}
