db3444cb2d3b2031d0c48cde6273029a68ee59b8
Alex Vulaj <avulaj@redhat.com>
2023-02-10 13:53:26 -0500

    Add commands to check if cluster owner is a banned user, and to verify that their pull secret matches the cluster's

---
+++ cmd/cluster/validatepullsecret_test.go
@@ -0,0 +1,52 @@
+package cluster
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"reflect"
+	"testing"
+)
+
+func Test_getPullSecretEmail(t *testing.T) {
+	tests := []struct {
+		name          string
+		secret        *corev1.Secret
+		expectedEmail string
+		expectedError error
+		expectedDone  bool
+	}{
+		{
+			name:         "Missing dockerconfigjson",
+			secret:       &corev1.Secret{Data: map[string][]byte{}},
+			expectedDone: true,
+		},
+		{
+			name:         "Missing cloud.openshift.com auth",
+			secret:       &corev1.Secret{Data: map[string][]byte{".dockerconfigjson": []byte("{\"auths\":{}}")}},
+			expectedDone: true,
+		},
+		{
+			name:         "Missing email",
+			secret:       &corev1.Secret{Data: map[string][]byte{".dockerconfigjson": []byte("{\"auths\":{\"cloud.openshift.com\":{}}}")}},
+			expectedDone: true,
+		},
+		{
+			name:          "Valid pull secret",
+			secret:        &corev1.Secret{Data: map[string][]byte{".dockerconfigjson": []byte("{\"auths\":{\"cloud.openshift.com\":{\"email\":\"foo@bar.com\"}}}")}},
+			expectedEmail: "foo@bar.com",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			email, err, done := getPullSecretEmail("abc123", tt.secret)
+			if email != tt.expectedEmail {
+				t.Errorf("getPullSecretEmail() email = %v, expectedEmail %v", email, tt.expectedEmail)
+			}
+			if !reflect.DeepEqual(err, tt.expectedError) {
+				t.Errorf("getPullSecretEmail() err = %v, expectedEmail %v", err, tt.expectedError)
+			}
+			if done != tt.expectedDone {
+				t.Errorf("getPullSecretEmail() done = %v, expectedEmail %v", done, tt.expectedDone)
+			}
+		})
+	}
+}
