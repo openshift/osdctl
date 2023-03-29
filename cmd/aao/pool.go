4384a19f06bb3ed3bf295e6051352974f4129704
Dan Keohane <dkeohane@redhat.com>
2023-03-29 11:30:43 +0200

    Update output

--- cmd/aao/pool.go
+++ cmd/aao/pool.go
@@ -3,9 +3,12 @@ package aao
 import (
 	"context"
 	"fmt"
+	"os"
 	"sort"
+	"strconv"
 
 	awsv1alpha1 "github.com/openshift/aws-account-operator/api/v1alpha1"
+	"github.com/openshift/osdctl/pkg/printer"
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
@@ -46,6 +49,14 @@ func (o *poolOptions) complete(cmd *cobra.Command) error {
 	return nil
 }
 
+type legalEntityStats struct {
+	name         string
+	id           string
+	claimedCount int
+	unusedCount  int
+	totalCount   int
+}
+
 func (o *poolOptions) run() error {
 	ctx := context.TODO()
 	var accounts awsv1alpha1.AccountList
@@ -56,60 +67,81 @@ func (o *poolOptions) run() error {
 	}
 
 	// mapping legalentityid to count
-	defaultMap := make(map[string]int)
-	fmMap := make(map[string]int)
+	defaultMap := make(map[string]legalEntityStats)
+	fmMap := make(map[string]legalEntityStats)
 	availabilityCount := 0
+
 	for _, account := range accounts.Items {
 
-		if !account.Status.Claimed && account.Status.State == "Ready" && account.Spec.LegalEntity.ID == "" {
+		if !account.Status.Claimed && account.Status.State == "Ready" && account.Spec.LegalEntity.ID == "" && !account.Spec.BYOC {
 			availabilityCount += 1
 		}
 
-		if account.Spec.LegalEntity.ID == "" {
+		if account.Spec.LegalEntity.ID == "" || account.Spec.BYOC {
 			continue
 		}
 
-		key := fmt.Sprintf("%s %s", account.Spec.LegalEntity.ID, account.Spec.LegalEntity.Name)
 		if account.Spec.AccountPool == "fm-accountpool" { // non-default accountpool
-			fmMap[key] += 1
+			handlePoolCounting(fmMap, account)
 		} else {
-			defaultMap[key] += 1
+			handlePoolCounting(defaultMap, account)
 		}
 	}
 	fmt.Printf("Available Accounts: %d\n", availabilityCount)
-	fmt.Println("============================================================")
+	fmt.Println("========================================================================================================================")
 	fmt.Println("Default Account Pool")
-	fmt.Println("============================================================")
+	fmt.Println("========================================================================================================================")
 	printSortedCount(getSortedCount(defaultMap, 10))
-	fmt.Println()
 
-	fmt.Println("============================================================")
+	fmt.Println("========================================================================================================================")
 	fmt.Println("fm-accountpool")
-	fmt.Println("============================================================")
+	fmt.Println("========================================================================================================================")
 	printSortedCount(getSortedCount(fmMap, 10))
-	fmt.Println()
 
 	return nil
 }
 
-type legalEntityCount struct {
-	legalEntity string
-	count       int
+func handlePoolCounting(myMap map[string]legalEntityStats, account awsv1alpha1.Account) {
+	key := fmt.Sprintf("%s %s", account.Spec.LegalEntity.ID, account.Spec.LegalEntity.Name)
+	if account.Status.Claimed {
+		if entry, ok := myMap[key]; ok {
+			entry.claimedCount += 1
+			myMap[key] = entry
+		} else {
+			myMap[key] = legalEntityStats{
+				name:         account.Spec.LegalEntity.Name,
+				id:           account.Spec.LegalEntity.ID,
+				claimedCount: 1,
+			}
+		}
+	} else {
+		if entry, ok := myMap[key]; ok {
+			entry.unusedCount += 1
+			myMap[key] = entry
+		} else {
+			myMap[key] = legalEntityStats{
+				name:        account.Spec.LegalEntity.Name,
+				id:          account.Spec.LegalEntity.ID,
+				unusedCount: 1,
+			}
+		}
+	}
 }
 
-func mapToCountArray(input map[string]int) []legalEntityCount {
-	result := make([]legalEntityCount, 0)
-	for k, v := range input {
-		result = append(result, legalEntityCount{legalEntity: k, count: v})
+func prepareLESMapToArray(input map[string]legalEntityStats) []legalEntityStats {
+	result := make([]legalEntityStats, 0)
+	for _, v := range input {
+		v.totalCount = v.claimedCount + v.unusedCount
+		result = append(result, v)
 	}
 
 	return result
 }
 
-func getSortedCount(inputMap map[string]int, maxLen int) []legalEntityCount {
-	inputCounts := mapToCountArray(inputMap)
+func getSortedCount(inputMap map[string]legalEntityStats, maxLen int) []legalEntityStats {
+	inputCounts := prepareLESMapToArray(inputMap)
 	sort.Slice(inputCounts, func(i, j int) bool {
-		return inputCounts[i].count > inputCounts[j].count
+		return inputCounts[i].totalCount > inputCounts[j].totalCount
 	})
 
 	limit := len(inputCounts)
@@ -120,8 +152,35 @@ func getSortedCount(inputMap map[string]int, maxLen int) []legalEntityCount {
 	return inputCounts[:maxLen]
 }
 
-func printSortedCount(lec []legalEntityCount) {
+func printSortedCount(lec []legalEntityStats) {
+
+	totalClaimed := 0
+	totalUnused := 0
+	totalTotal := 0
+
+	table := printer.NewTablePrinter(os.Stdout, 20, 1, 3, ' ')
+	table.AddRow([]string{"Claimed", "Unused", "Total", "ID", "Name"})
 	for i := range lec {
-		fmt.Printf("%d:\t%s \n", lec[i].count, lec[i].legalEntity)
+		table.AddRow([]string{
+			strconv.Itoa(lec[i].claimedCount),
+			strconv.Itoa(lec[i].unusedCount),
+			strconv.Itoa(lec[i].totalCount),
+			lec[i].id, lec[i].name,
+		})
+		totalClaimed += lec[i].claimedCount
+		totalUnused += lec[i].unusedCount
+		totalTotal += lec[i].totalCount
 	}
+
+	// Add empty row for readability
+	table.AddRow([]string{})
+	err := table.Flush()
+	if err != nil {
+		fmt.Println("error while flushing table: ", err.Error())
+	}
+
+	fmt.Printf("Total Claimed: %d\n", totalClaimed)
+	fmt.Printf("Total Unused: %d\n", totalUnused)
+	fmt.Printf("Total Accounts: %d\n", totalTotal)
+	fmt.Println()
 }
