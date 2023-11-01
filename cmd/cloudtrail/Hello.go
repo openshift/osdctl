082a7e20550fd1a3b5c8e15616a8254da12a9511
TomNelsonTembo <neltembo2002@gmail.com>
2023-11-01 14:20:37 +0100

    made changes to hello.go

--- cmd/cloudtrail/Hello.go
+++ cmd/cloudtrail/Hello.go
@@ -12,13 +12,8 @@ import (
 // HelloCmd represents the Hello command
 var helloCmd = &cobra.Command{
 	Use:   "Hello",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints out Hello Cloundtrail",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello Cloudtrail")
 	},
