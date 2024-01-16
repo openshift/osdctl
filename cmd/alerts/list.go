9a1ef45bb6050dbcbfbe47be70af8411a15c525d
Mitul Sharma <misharma@redhat.com>
2024-01-16 11:47:56 +0530

    run oc exec cmd with url

--- cmd/alerts/list.go
+++ cmd/alerts/list.go
@@ -1,20 +1,45 @@
 package alerts
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	routev1 "github.com/openshift/api/route/v1"
 	"github.com/openshift/backplane-cli/cmd/ocm-backplane/login"
 	"github.com/openshift/backplane-cli/pkg/cli/config"
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/kubectl/pkg/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const ContainerName string = "prometheus"
+
+var accountNamespace string = "openshift-monitoring"
+var alertprom string = "alertmanager-main"
+var PodName string = "prometheus-k8s-0"
+
+type logCapture struct {
+	buffer bytes.Buffer
+}
+
+func (capture *logCapture) GetStdOut() string {
+	return capture.buffer.String()
+}
+
+func (capture *logCapture) Write(p []byte) (n int, err error) {
+	a := string(p)
+	capture.buffer.WriteString(a)
+	return len(p), nil
+}
 
 func NewCmdList() *cobra.Command {
 	return &cobra.Command{
@@ -30,17 +55,13 @@ func NewCmdList() *cobra.Command {
 }
 
 func ListCheck(clusterID string) {
-
-	var accountNamespace string = "openshift-monitoring"
-	var alertprom string = "alertmanager-main"
-
 	defer func() {
 		if err := recover(); err != nil {
 			log.Fatal("error : ", err)
 		}
 	}()
 
-	kClient, _, err := getKubeCli(clusterID)
+	kClient, kubeconfig, clientset, err := getKubeCli(clusterID)
 	if err != nil{
 		log.Fatal(err)
 	}
@@ -60,11 +81,24 @@ func ListCheck(clusterID string) {
 	 	fmt.Println("Could not retrieve desired alertmanager-main route.")
 	 	return
 	}
-	fmt.Printf("Retrieved route to host: %s\n", route.Spec.Host)
+
+	/*fmt.Printf("Retrieved route to host: %s\n", route.Spec.Host)
+	posturl := "/api/v1/alerts"
+	routeurl := route.Spec.Host
+	url :=  routeurl + posturl
+	fmt.Printf("Retrieved route to host: %s\n", url)*/
+	url := "http://localhost:9090/api/v1/alerts"
+
+	output, err := getAlerts(kubeconfig, clientset, PodName, url)
+	if err != nil {
+		fmt.Println("Could not execute the oc exec command")
+		return
+   	}
+	fmt.Println(output)
     
 }
 
-func getKubeCli(clusterID string) (client.Client, *rest.Config , error) {
+func getKubeCli(clusterID string) (client.Client, *rest.Config , *kubernetes.Clientset, error) {
 
 	scheme := runtime.NewScheme()
 	err := routev1.AddToScheme(scheme) // added to scheme
@@ -72,7 +106,7 @@ func getKubeCli(clusterID string) (client.Client, *rest.Config , error) {
  		fmt.Print("failed to register scheme")
  	}
 
-  	bp, err := config.GetBackplaneConfiguration()
+	bp, err := config.GetBackplaneConfiguration()
 	if err != nil {
 		log.Fatalf("failed to load backplane-cli config: %v", err)
 	}
@@ -82,11 +116,71 @@ func getKubeCli(clusterID string) (client.Client, *rest.Config , error) {
  		log.Fatalf("failed to load backplane admin: %v", err)
  	}
 
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		log.Fatalf("failed to create clientset : %v", err)
+	}
+
 	kubeCli, err := client.New(kubeconfig, client.Options{})
 	if err != nil {
 		log.Fatalf("failed to load kubecli : %v", err)
 	}
 
-	return kubeCli, kubeconfig, err
+	return kubeCli, kubeconfig, clientset, err
+}
+
+//c.Println(fmt.Sprintf("Use \n\n    oc exec -it --as %s -n %s %s -- /bin/bash\n\nto run commands in the pod. All 'oc' commands run within the pod will be executed against the cluster '%s' (this can be verified by running `oc cluster-info` in the pod)" pod.Namespace, pod.Name)
+//oc -n openshift-monitoring exec -c prometheus prometheus-k8s-0 -- curl -s   'http://localhost:9090/api/v1/alerts'
+
+func getAlerts(kubeconfig *rest.Config, clientset *kubernetes.Clientset, PodName string, url string) (string, error) {
+
+	cmd := []string{
+		"sh",
+		"-c",
+		"-- curl",
+		"-s",
+	}
+	req := clientset.CoreV1().RESTClient().Post().Resource("pods").Name(PodName).
+		Namespace(accountNamespace).RequestURI(url).SubResource("exec")
+	option := &corev1.PodExecOptions{
+		Container: ContainerName,
+		Command:   cmd,
+		Stdin:     true,
+		Stdout:    true,
+		Stderr:    true,
+		TTY:       false,
+	}
+
+	if os.Stdin == nil {
+		option.Stdin = false
+	}
+	req.VersionedParams(
+		option,
+		scheme.ParameterCodec,
+	)
+
+
+	exec, err := remotecommand.NewSPDYExecutor(kubeconfig, "POST", req.URL())
+	if err != nil {
+		return "", err
+	}
+
+	capture := &logCapture{}
+	errorCapture := &logCapture{}
+
+	err = exec.StreamWithContext(context.TODO(), remotecommand.StreamOptions{
+		Stdin:  bytes.NewReader([]byte{}),
+		Stdout: capture,
+		Stderr: errorCapture,
+		Tty:    false,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	cmdOutput := capture.GetStdOut()
+	return cmdOutput, nil
 }
 
