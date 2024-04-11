d2eabba74b73bd88083bd279dd37fb44287eb8e0
Mitul Sharma <misharma@redhat.com>
2024-04-11 09:21:37 +0530

    added code comment in common.go

--- cmd/alerts/silence/common.go
+++ cmd/alerts/silence/common.go
@@ -18,11 +18,13 @@ const (
 	PodName          = "alertmanager-main-0"
 )
 
+// ExecInPod is designed to execute a command inside a Kubernetes pod and capture its output.
 func ExecInPod(kubeconfig *rest.Config, clientset *kubernetes.Clientset, cmd []string) (string, error) {
-
+	// request is constructed using the Kubernetes clientset to interact with the Kubernetes API.
+	// It specifies that the request is for executing a command inside a pod (SubResource("exec")).
 	req := clientset.CoreV1().RESTClient().Post().Resource("pods").Name(PodName).
 		Namespace(AccountNamespace).SubResource("exec")
-
+	//various options for executing the command inside the pod
 	option := &corev1.PodExecOptions{
 		Container: ContainerName,
 		Command:   cmd,
@@ -33,6 +35,8 @@ func ExecInPod(kubeconfig *rest.Config, clientset *kubernetes.Clientset, cmd []s
 	}
 
 	req.VersionedParams(option, scheme.ParameterCodec)
+	//SPDY executor is created using the Kubernetes configuration and the request URL.
+	//This executor will be responsible for executing the command inside the pod.
 
 	exec, err := remotecommand.NewSPDYExecutor(kubeconfig, "POST", req.URL())
 	if err != nil {
@@ -41,6 +45,8 @@ func ExecInPod(kubeconfig *rest.Config, clientset *kubernetes.Clientset, cmd []s
 
 	capture := &cluster.LogCapture{}
 	errorCapture := &cluster.LogCapture{}
+	//capture and errorCapture are instances of cluster.LogCapture,
+	//which are used to capture the stdout and stderr output of the executed command.
 
 	err = exec.StreamWithContext(context.TODO(), remotecommand.StreamOptions{
 		Stdin:  nil,
