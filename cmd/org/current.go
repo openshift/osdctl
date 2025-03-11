831b183000bc2d0155c157d0dcfab94fff8198c2
Amit Upadhyay <amiupadh@redhat.com>
2025-03-12 01:35:26 +0530

    Refactor getCurrentOrg for better testability

--- cmd/org/current.go
+++ cmd/org/current.go
@@ -3,7 +3,6 @@ package org
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/openshift-online/ocm-cli/pkg/arguments"
 	sdk "github.com/openshift-online/ocm-sdk-go"
@@ -18,59 +17,56 @@ var (
 		Short:         "gets current organization",
 		Args:          cobra.ArbitraryArgs,
 		SilenceErrors: true,
-		Run: func(cmd *cobra.Command, args []string) {
+		Run: func(_ *cobra.Command, _ []string) {
 			ocmClient, err := utils.CreateConnection()
 			if err != nil {
 				cmdutil.CheckErr(err)
 			}
 			defer func() {
 				if err := ocmClient.Close(); err != nil {
-					fmt.Printf("Cannot close the ocmClient (possible memory leak): %q", err)
+					cmdutil.CheckErr(fmt.Errorf("cannot close the ocmClient (possible memory leak): %q", err))
 				}
 			}()
-		
-			org, err := run(cmd, ocmClient)
+			req, err := getOrgRequest(ocmClient)
 			if err != nil {
 				cmdutil.CheckErr(err)
 			}
-			printOrg(org) 
+			resp, err := sendRequest(req)
+			if err != nil {
+				cmdutil.CheckErr(fmt.Errorf("invalid input: %q", err))
+			}
+			orgs, err := getCurrentOrg(resp.Bytes())
+			if err != nil {
+				cmdutil.CheckErr(err)
+			}
+			printOrg(*orgs)
 		},
 	}
 )
 
-type Account struct {
+type account struct {
 	Organization Organization `json:"organization"`
 }
 
 func init() {
 	flags := currentCmd.Flags()
-
 	AddOutputFlag(flags)
 }
 
-func run(cmd *cobra.Command, ocmClient *sdk.Connection) (Organization, error) {
-	response, err := getCurrentOrg(ocmClient)
+func getCurrentOrg(data []byte) (*Organization, error) {
+	acc := account{}
+	err := json.Unmarshal(data, &acc)
 	if err != nil {
-		return Organization{}, fmt.Errorf("invalid input: %q", err)
+		return nil, err
 	}
-
-	acc := Account{}
-	if err := json.Unmarshal(response.Bytes(), &acc); err != nil {
-		return Organization{}, fmt.Errorf("failed to parse response: %w", err)
-	}
-
-	return acc.Organization, nil
+	return &acc.Organization, nil
 }
 
-func getCurrentOrg(ocmClient *sdk.Connection) (*sdk.Response, error) {
-	return sendRequest(createGetCurrentOrgRequest(ocmClient))
-}
-
-func createGetCurrentOrgRequest(ocmClient *sdk.Connection) *sdk.Request {
-	request := ocmClient.Get()
-	err := arguments.ApplyPathArg(request, currentAccountApiPath)
+func getOrgRequest(ocmClient *sdk.Connection) (*sdk.Request, error) {
+	req := ocmClient.Get()
+	err := arguments.ApplyPathArg(req, currentAccountApiPath)
 	if err != nil {
-		log.Fatalf("Can't parse API path '%s': %v\n", currentAccountApiPath, err)
+		return nil, fmt.Errorf("can't parse API path '%s': %v\n", currentAccountApiPath, err)
 	}
-	return request
+	return req, nil
 }
--- cmd/org/current_test.go
+++ cmd/org/current_test.go
@@ -1,60 +1,61 @@
 package org
 
 import (
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	sdk "github.com/openshift-online/ocm-sdk-go"
+	"github.com/openshift/osdctl/pkg/utils"
 )
 
-func Test_run(t *testing.T) {
-	t.Run("Success Test", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
+var userData = `
+{
+    "created_at": "2024-11-27T07:32:59.368563Z",
+    "email": "kilgore.trout@redhat.com",
+    "first_name": "Kilgore",
+    "href": "/api/accounts_mgmt/v1/accounts/xyz",
+    "id": "foobar",
+    "kind": "Account",
+    "last_name": "Trout",
+    "organization": {
+        "created_at": "2019-02-15T20:26:12.542449Z",
+        "ebs_account_id": "1111111",
+        "external_id": "2222222",
+        "href": "/api/accounts_mgmt/v1/organizations/xyz",
+        "id": "abc.xyz",
+        "kind": "Organization",
+        "name": "Kurt Vonnegut Appreciation Society",
+        "updated_at": "2025-03-10T06:16:08.047253Z"
+    },
+    "rhit_web_user_id": "57712380",
+    "updated_at": "2025-02-21T21:05:50.544761Z",
+    "username": "kilgore.trout"
+}
+`
 
-			if r.URL.Path == "/oauth2/token" {
-				w.Write([]byte(`{"access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0LXVzZXIiLCJleHAiOjI1MjQ2MDgwMDB9.signature-placeholder", "token_type": "Bearer", "expires_in": 3600}`))
-				return
-			}
-
-			response := map[string]interface{}{
-				"organization": map[string]string{
-					"id":          "org-123",
-					"external_id": "external-123",
-					"name":        "Test Organization",
-				},
-			}
-			json.NewEncoder(w).Encode(response)
-		}))
-		defer server.Close()
-
-		conn, err := sdk.NewConnectionBuilder().
-			URL(server.URL).
-			TokenURL(server.URL + "/oauth2/token").
-			Insecure(true).
-			Client("fake-id", "fake-secret").
-			Build()
-		if err != nil {
-			t.Fatalf("Failed to build connection: %v", err)
-		}
-
-		org, err := run(nil, conn)
-		if err != nil {
-			t.Errorf("run() returned an error: %v", err)
-		}
-
-		expected := Organization{
-			ID:         "org-123",
-			ExternalID: "external-123",
-			Name:       "Test Organization",
-		}
-
-		if org != expected {
-			t.Errorf("Expected %+v, got %+v", expected, org)
-		}
-	})
+func TestGetCurrentOrg(t *testing.T) {
+	orgs, err := getCurrentOrg([]byte(userData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	name := "Kurt Vonnegut Appreciation Society"
+	if orgs.Name != name {
+		t.Errorf("Expected %s to equal %s", orgs.Name, name)
+	}
+	id := "abc.xyz"
+	if orgs.ID != id {
+		t.Errorf("Expected %s to equal %s", orgs.ID, id)
+	}
 }
 
+func TestGetOrgRequest(t *testing.T) {
+	ocmClient, err := utils.CreateConnection()
+	if err != nil {
+		t.Fatal(err)
+	}
+	req, err := getOrgRequest(ocmClient)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if req.GetPath() != currentAccountApiPath {
+		t.Errorf("%s does not equal %s", req.GetPath(), currentAccountApiPath)
+	}
+}
