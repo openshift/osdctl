59c274f7a5f7bf4cf197dd156dc64d5b8487d9e7
erosente <erosente@redhat.com>
2025-03-13 16:35:15 -0400

    better wording

--- cmd/network/packet-capture.go
+++ cmd/network/packet-capture.go
@@ -530,6 +530,6 @@ func setCaptureInterface(o *packetCaptureOptions) error {
 		o.captureInterface = "vxlan_sys_4789"
 		return nil
 	default:
-		return fmt.Errorf("failed to determine network type or unknown network type type %s unknown", networkConfig.Spec.NetworkType)
+		return fmt.Errorf("failed to determine network type. Network type %s unknown", networkConfig.Spec.NetworkType)
 	}
 }
