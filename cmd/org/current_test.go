64fb3a860d240b9eec2ca70a03da945f80ed1489
Amit Upadhyay <amiupadh@redhat.com>
2025-03-26 23:46:54 +0530

    Encapsulated user test data into function

--- cmd/org/current_test.go
+++ cmd/org/current_test.go
@@ -1,6 +1,8 @@
 package org
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,32 +10,41 @@ import (
 	sdk "github.com/openshift-online/ocm-sdk-go"
 )
 
-var userData = `
-{
-    "created_at": "2024-11-27T07:32:59.368563Z",
-    "email": "kilgore.trout@redhat.com",
-    "first_name": "Kilgore",
-    "href": "/api/accounts_mgmt/v1/accounts/xyz",
-    "id": "foobar",
-    "kind": "Account",
-    "last_name": "Trout",
-    "organization": {
-        "created_at": "2019-02-15T20:26:12.542449Z",
-        "ebs_account_id": "1111111",
-        "external_id": "2222222",
-        "href": "/api/accounts_mgmt/v1/organizations/xyz",
-        "id": "abc.xyz",
-        "kind": "Organization",
-        "name": "Kurt Vonnegut Appreciation Society",
-        "updated_at": "2025-03-10T06:16:08.047253Z"
-    },
-    "rhit_web_user_id": "57712380",
-    "updated_at": "2025-02-21T21:05:50.544761Z",
-    "username": "kilgore.trout"
+func getUserTestData() (string, error) {
+	data := map[string]interface{}{
+		"created_at": "2024-11-27T07:32:59.368563Z",
+		"email":      "kilgore.trout@redhat.com",
+		"first_name": "kilgore",
+		"href":       "/api/accounts_mgmt/v1/accounts/xyz",
+		"id":         "foobar",
+		"kind":       "Account",
+		"last_name":  "Trout",
+		"organization": map[string]interface{}{
+			"created_at":     "2019-02-15T20:26:12.542449Z",
+			"ebs_account_id": "1111111",
+			"external_id":    "2222222",
+			"href":           "/api/accounts_mgmt/v1/organizations/xyz",
+			"id":             "abc.xyz",
+			"kind":           "Organization",
+			"name":           "Kurt Vonnegut Appreciation Society",
+			"updated_at":     "2025-03-10T06:16:08.047253Z",
+		},
+		"rhit_web_user_id": "57712380",
+		"updated_at":       "2025-02-21T21:05:50.544761Z",
+		"username":         "kilgore.trout",
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal test data: %w", err)
+	}
+	return string(jsonData), nil
 }
-`
 
 func TestGetCurrentOrg(t *testing.T) {
+	userData, err := getUserTestData()
+	if err != nil {
+		t.Errorf("invalid user test data")
+	}
 	orgs, err := getCurrentOrg([]byte(userData))
 	if err != nil {
 		t.Fatal(err)
@@ -55,6 +66,10 @@ func TestGetOrgRequest(t *testing.T) {
 		if r.URL.Path == tokenPath {
 			w.Write([]byte(`{"access_token": "test-token"}`))
 			return
+		}
+		userData, err := getUserTestData()
+		if err != nil {
+			t.Errorf("invalid user test data")
 		}
 		w.Write([]byte(userData))
 	}))
