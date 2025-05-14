a5a88ea34b006303e662322f4a95aff116ea74db
Josh Branham <jbranham@redhat.com>
2025-05-14 09:36:29 -0600

    Fix issues with `osdctl promote saas` In https://github.com/openshift/osdctl/pull/723 a refactor was done to make testing easier for this package. However, it turns out that was not tested functionally and there were a few issues. This fixes those issues.

--- cmd/promote/git/service_repo.go
+++ cmd/promote/git/service_repo.go
@@ -9,7 +9,7 @@ import (
 	"github.com/openshift/osdctl/cmd/promote/iexec"
 )
 
-func CheckoutAndCompareGitHash(gitexecuter iexec.IExec, gitURL, gitHash, currentGitHash string) (string, string, error) {
+func CheckoutAndCompareGitHash(gitExecutor iexec.IExec, gitURL, gitHash, currentGitHash string) (string, string, error) {
 	tempDir, err := ioutil.TempDir("", "")
 	if err != nil {
 		return "", "", fmt.Errorf("failed to create temporary directory: %v", err)
@@ -21,7 +21,7 @@ func CheckoutAndCompareGitHash(gitexecuter iexec.IExec, gitURL, gitHash, current
 		return "", "", fmt.Errorf("failed to change directory to temporary directory: %v", err)
 	}
 
-	err = gitexecuter.Run(tempDir, "git", "clone", gitURL, "source-dir")
+	err = gitExecutor.Run(tempDir, "git", "clone", gitURL, "source-dir")
 	if err != nil {
 		return "", "", fmt.Errorf("failed to clone git repository: %v", err)
 	}
@@ -32,21 +32,21 @@ func CheckoutAndCompareGitHash(gitexecuter iexec.IExec, gitURL, gitHash, current
 
 	if gitHash == "" {
 		fmt.Printf("No git hash provided. Using HEAD.\n")
-		output, err := gitexecuter.Output("git", "rev-parse", "HEAD")
+		output, err := gitExecutor.Output("", "git", "rev-parse", "HEAD")
 		if err != nil {
 			return "", "", fmt.Errorf("failed to get git hash: %v", err)
 		}
-		gitHash = strings.TrimSpace(string(output))
+		gitHash = strings.TrimSpace(output)
 		fmt.Printf("The head githash is %s\n", gitHash)
 	}
 
 	if currentGitHash == gitHash {
 		return "", "", fmt.Errorf("git hash %s is already at HEAD", gitHash)
 	} else {
-		commitLog, err := gitexecuter.Output("git", "log", "--no-merges", fmt.Sprintf("%s..%s", currentGitHash, gitHash))
+		commitLog, err := gitExecutor.Output("", "git", "log", "--no-merges", fmt.Sprintf("%s..%s", currentGitHash, gitHash))
 		if err != nil {
 			return "", "", err
 		}
-		return gitHash, string(commitLog), nil
+		return gitHash, commitLog, nil
 	}
 }
