6e19026e7989fc61fb778b973c8a2c663613f65c
evlin-rh <evlin@redhat.com>
2025-05-20 16:19:44 +0100

    Test

--- cmd/cloudtrail/cmd.go
+++ cmd/cloudtrail/cmd.go
@@ -4,16 +4,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// If *cobra.command is empty, return man page for Write and PermissionDenied (?)
+
 // NewCloudtrailCmd represents the newCmdWriteEvents command
-func NewCloudtrailCmd() *cobra.Command {
+
+func NewCloudtrailCmd() *cobra.Command { //returns pointer for cobra.Command
+	//Known as composite literals
+	//cobra.Command is a struct that consist of Use:string, Short:string and Run -> runs a function
+	// & allows you to set values of the keys in cobra.Command
 	cloudtrailCmd := &cobra.Command{
 		Use:   "cloudtrail",
 		Short: "AWS CloudTrail related utilities",
-		Run: func(cmd *cobra.Command, args []string) {
+		Run: func(cmd *cobra.Command, args []string) { //runs a function cmd  that takes string list as args
 			cmd.Help()
 		},
 	}
 
+	// Adds new writeEvents or PermissionDenied to both files respectively
 	cloudtrailCmd.AddCommand(newCmdWriteEvents())
 	cloudtrailCmd.AddCommand(newCmdPermissionDenied())
 
